62763cf97442173ae70560ce190fc3a10ba8b02d
Anton Vietrov <avietrov@users.noreply.github.com>
2022-10-15 15:07:18 +0200

    Implement dynamic colors.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::ops::Range;
-
+use colors_transform::{Hsl, Color};
 use image::{RgbImage, Rgb};
-use indicatif::ProgressBar;
 use itertools::{Itertools, Product};
 use newton_factal::math::complex::Complex;
 use newton_factal::math::polynomial::Polynomial;
 use newton_factal::newton_method_approximate;
-
+use std::f64::consts::PI;
 
 struct Field {
     source: (u32, u32),
@@ -32,41 +31,63 @@ impl Field {
     }
 }
 
+struct RootPoint {
+    root: Complex,
+    iter: u32
+}
 
-fn main() {
-    let pol = Polynomial::new(vec![-1, 0, 0, 1]);
-    let colors = [
-        Rgb([0, 0, 0]),
-        Rgb([255, 0, 0]),
-        Rgb([0, 255, 0]),
-        Rgb([0, 0, 255]),
-        Rgb([255, 255, 0]),
-        Rgb([255, 0, 255]),
-        Rgb([0, 255, 255]),
-        Rgb([255, 255, 255]),
-        Rgb([155, 155, 155]),
-        Rgb([55, 55, 55]),
-    ];
+fn clamp01(v: f64) -> f64 {
+    f64::min(f64::max(0., v), 1.)
+}
 
-    let w: u32 = 512;
-    let pb = ProgressBar::new(w as u64 * w as u64);
-    let mut image = RgbImage::new(w, w);
+fn hsl_to_rgb(color: Hsl) -> Rgb<u8> {
+    Rgb([
+        (color.get_red() ) as u8,
+        (color.get_green() ) as u8,
+        (color.get_blue() ) as u8,
+        ])
+}
 
-    let field = Field {
-        source: (0, 0),
-        ssize: w,
+fn color_from_root(roots: &Vec<Complex>, root: RootPoint) -> Hsl {
+    for r in roots {
+        if *r == root.root {
+            let hue = clamp01(f64::abs(0.5 - root.root.arg() / (PI * 2.))) * 360.;
+            let sat = clamp01(f64::abs(0.59 / root.root.abs())) * 100.;
+            let lum = 0.95 * f64::max(1.0 - root.iter as f64 * 0.025, 0.05) * 100.;
+            return Hsl::from(hue as f32, sat as f32, lum as f32);
+        }
+    }
 
-        target: (-0.5, -0.5),
-        tsize: 5.0,
-    };
+    Hsl::from(0., 0., 0.)
+}
 
+fn render_image(pol: Polynomial, field: Field) -> RgbImage {
+    let mut image = RgbImage::new(field.ssize, field.ssize);
+    let mut roots = vec![];
     for (i, j) in field.iterate() {
         let (re, im) = field.project((i, j));
         let point = Complex { re, im };
-        let (root, iter) = newton_method_approximate(&pol, point, 9);
-        let color = colors[(iter % colors.len() as u32) as usize];
-        image.put_pixel(i as u32, j as u32, color);
-        pb.inc(1);
+        let (root, iter) = newton_method_approximate(&pol, point, 100);
+        if !roots.contains(&root) {
+            roots.push(root);
+        }
+        let rp = RootPoint { root, iter };
+        let color = color_from_root(&roots, rp);
+        image.put_pixel(i, j, hsl_to_rgb(color));
     }
+
+    return image;
+}
+
+fn main() {
+    let pol = Polynomial::new(vec![-1, 0, 0, 1]);
+    let field = Field {
+        source: (0, 0),
+        ssize: 512,
+
+        target: (-5., -5.),
+        tsize: 10.0,
+    };
+    let image = render_image(pol, field);
     image.save("output.png").unwrap();
 }
