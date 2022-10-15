768454d72527f5e4b9ccdc5a479c1918e128c770
Anton Vietrov <avietrov@users.noreply.github.com>
2022-10-15 16:27:16 +0200

    Improve benchmarks.

--- benches/rendering_benchmark.rs
+++ benches/rendering_benchmark.rs
@@ -1,19 +1,27 @@
+use std::time::Duration;
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use newton_factal::{render_image, Field};
 use newton_factal::math::polynomial::Polynomial;
+use criterion::BenchmarkId;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("render", |b| b.iter(|| {
-        let pol = Polynomial::new(vec![3, 5, 0, 7, -3, 2]); // y = 2x^5 - 3x^4 + 7x^3 + 5x + 3
-        let field = Field {
-            source: (0, 0),
-            ssize: 512,
+    let mut group = c.benchmark_group("render");
+    group.sample_size(10).measurement_time(Duration::from_secs(10));
+    for size in [32, 64, 128, 256, 512] {
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| b.iter(|| {
+            let pol = Polynomial::new(vec![3, 5, 0, 7, -3, 2]); // y = 2x^5 - 3x^4 + 7x^3 + 5x + 3
+            let field = Field {
+                source: (0, 0),
+                ssize: size,
 
-            target: (-5., -5.),
-            tsize: 10.0,
-        };
-        render_image(black_box(pol), black_box(field));
-    }));
+                target: (-5., -5.),
+                tsize: 10.0,
+            };
+            render_image(black_box(pol), black_box(field));
+        }));
+    }
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/solution_benchmark.rs
+++ benches/solution_benchmark.rs
@@ -4,10 +4,10 @@ use newton_factal::math::polynomial::Polynomial;
 use newton_factal::math::complex::Complex;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("pol 5", |b| b.iter(|| {
+    c.bench_function("approximate pol 5", |b| b.iter(|| {
         let pol = Polynomial::new(vec![3, 5, 0, 7, -3, 2]); // y = 2x^5 - 3x^4 + 7x^3 + 5x + 3
         let com = Complex { re: 4., im: 2.};
-        newton_method_approximate(black_box(&pol), black_box(com), 9);
+        newton_method_approximate(black_box(&pol), black_box(com), 100);
     }));
 }
 
