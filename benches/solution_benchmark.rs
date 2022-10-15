06f3072daf382b25fcc0efe723bb793e0e399255
Anton Vietrov <avietrov@users.noreply.github.com>
2022-10-15 12:37:38 +0200

    Implement simple benchmark.

---
+++ benches/solution_benchmark.rs
@@ -0,0 +1,15 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use newton_factal::newton_method_approximate;
+use newton_factal::math::polynomial::Polynomial;
+use newton_factal::math::complex::Complex;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("pol 5", |b| b.iter(|| {
+        let pol = Polynomial::new(vec![3, 5, 0, 7, -3, 2]); // y = 2x^5 - 3x^4 + 7x^3 + 5x + 3
+        let com = Complex { re: 4., im: 2.};
+        newton_method_approximate(black_box(&pol), black_box(com));
+    }));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
