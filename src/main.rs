64483cb280c40388907c691b598d72e1f364347c
Anton Vietrov <avietrov@users.noreply.github.com>
2022-10-29 22:00:39 +0200

    Parse parameters from the query params.

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,68 @@ use newton_factal::{render_image, Field};
 use std::io::Cursor;
 
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let pol = Polynomial::new(vec![-1, 0, 0, 1]);
-    let field = Field {
+async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            let params = req
+                .uri()
+                .query()
+                .map(|v| {
+                    url::form_urlencoded::parse(v.as_bytes())
+                        .into_owned()
+                        .collect()
+                })
+                .unwrap_or_else(HashMap::new);
+
+            let field = parse_field_params(&params);
+            let pol = parse_pol_param(&params);
+            let d = handle_image_request(pol, field);
+            *response.body_mut() = d.into();
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    }
+
+    Ok(response)
+}
+
+fn parse_field_params(params: &HashMap<String, String>) -> Field {
+    let tsize: f64 = params.get("tw").map(|s| s.parse().unwrap()).unwrap_or(10.0);
+    let tx: f64 = params.get("tx").map(|s| s.parse().unwrap()).unwrap_or(-5.0);
+    let ty: f64 = params.get("ty").map(|s| s.parse().unwrap()).unwrap_or(-5.0);
+
+    Field {
         source: (0, 0),
         ssize: 512,
 
-        target: (-5., -5.),
-        tsize: 10.0,
-    };
+        target: (tx, ty),
+        tsize: tsize,
+    }
+}
+
+fn parse_pol_param(params: &HashMap<String, String>) -> Polynomial {
+    let coef: Vec<i32> = params
+        .get("pol")
+        .map(|s| s.split(",").map(|s| s.parse().unwrap()).collect())
+        .unwrap();
+    Polynomial::new(coef)
+}
+
+fn handle_image_request(pol: Polynomial, field: Field) -> Vec<u8> {
     let image = render_image(pol, field);
     let mut data = Cursor::new(Vec::new());
     image
         .write_to(&mut data, image::ImageOutputFormat::Jpeg(255))
         .expect("Unable to write");
-    let d: Vec<u8> = data.get_ref().clone();
-    let mut response = Response::new(Body::empty());
-    *response.body_mut() = d.into();
-    Ok(response)
+    let d = data.get_ref().clone();
+    d
 }
 
 #[tokio::main]
