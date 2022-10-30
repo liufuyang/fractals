e8c6828933d0def28b32a392dd9a016b97e917b1
Anton Vietrov <avietrov@users.noreply.github.com>
2022-10-30 16:13:41 +0100

    Cleanup params parsing.

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,17 @@ use newton_factal::{render_image, Field};
 use std::io::Cursor;
 
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Method, Request, Response, Server, StatusCode, Uri};
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+
 
 async fn api(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
-            let params = req
-                .uri()
-                .query()
-                .map(|v| {
-                    url::form_urlencoded::parse(v.as_bytes())
-                        .into_owned()
-                        .collect()
-                })
-                .unwrap_or_else(HashMap::new);
-
+            let params = parse_params(req.uri());
             let field = parse_field_params(&params);
             let pol = parse_pol_param(&params);
             let d = handle_image_request(pol, field);
@@ -35,10 +27,21 @@ async fn api(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(response)
 }
 
+fn parse_params(uri: &Uri) -> HashMap<String, String> {
+    uri
+    .query()
+    .map(|v| {
+        url::form_urlencoded::parse(v.as_bytes())
+        .into_owned()
+        .collect()
+    }).unwrap_or_else(HashMap::new)
+
+}
+
 fn parse_field_params(params: &HashMap<String, String>) -> Field {
-    let tsize: f64 = params.get("tw").map(|s| s.parse().unwrap()).unwrap_or(10.0);
-    let tx: f64 = params.get("tx").map(|s| s.parse().unwrap()).unwrap_or(-5.0);
-    let ty: f64 = params.get("ty").map(|s| s.parse().unwrap()).unwrap_or(-5.0);
+    let tsize: f64 = parse_param_f64(params, "tw", 10.);
+    let tx: f64 = parse_param_f64(params, "tx", -5.);
+    let ty: f64 = parse_param_f64(params, "ty", -5.);
 
     Field {
         source: (0, 0),
@@ -47,6 +50,16 @@ fn parse_field_params(params: &HashMap<String, String>) -> Field {
         target: (tx, ty),
         tsize: tsize,
     }
+}
+
+fn parse_param_f64(params: &HashMap<String, String>, name: &str, default: f64) -> f64 {
+    if let Some(param) = params.get(name) {
+        if let Ok(val) = param.parse() {
+            return val;
+        }
+    }
+
+    default
 }
 
 fn parse_pol_param(params: &HashMap<String, String>) -> Polynomial {
