f0f44f4f0bf9bc61011217ae656f4160e8bcb1c7
Anton Vietrov <avietrov@users.noreply.github.com>
2022-10-30 21:18:34 +0100

    Improve benchmarks.

--- benches/rendering_benchmark.rs
+++ benches/rendering_benchmark.rs
@@ -1,27 +1,28 @@
-use std::time::Duration;
 
 use criterion::BenchmarkId;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use newton_factal::math::complex::Complex;
-use newton_factal::math::polynomial::Polynomial;
 use newton_factal::rendering::render_image;
-use newton_factal::{newton_method_field, Field};
+use newton_factal::{Field, Solution};
+use rand::prelude::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("render");
-    group
-        .sample_size(10)
-        .measurement_time(Duration::from_secs(10));
     for size in [32, 64, 128, 256, 512] {
         group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
             b.iter(|| {
-                let pol = Polynomial::new(vec![3, 5, 0, 7, -3, 2]); // y = 2x^5 - 3x^4 + 7x^3 + 5x + 3
+                // todo add seed to make the results comparible
+                let mut rng = rand::thread_rng();
+                let max_iter = 100;
+                let roots: Vec<Complex> = (0..5).map(|_| Complex { re: rng.gen(), im: rng.gen() }).collect();
+                let solutions = (0..(size*size))
+                    .map(|_| Solution {root: roots[rng.gen_range(0..roots.len())], iter: rng.gen_range(0..max_iter)})
+                    .collect();
                 let field = Field {
                     source: Complex { re: -5., im: -5. },
                     grid: size,
                     size: 10.0,
                 };
-                let solutions = newton_method_field(&pol, &field, 100);
                 render_image(&black_box(solutions), &black_box(field), 100);
             })
         });
--- benches/solution_benchmark.rs
+++ benches/solution_benchmark.rs
@@ -1,17 +1,45 @@
+use criterion::BenchmarkId;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use newton_factal::math::complex::Complex;
 use newton_factal::math::polynomial::Polynomial;
-use newton_factal::newton_method_approximate;
+use newton_factal::{newton_method_field, Field, newton_method_approximate};
+use rand::prelude::*;
+use std::time::Duration;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("approximate pol 5", |b| {
-        b.iter(|| {
-            let pol = Polynomial::new(vec![3, 5, 0, 7, -3, 2]); // y = 2x^5 - 3x^4 + 7x^3 + 5x + 3
-            let dpol = pol.derivative();
-            let com = Complex { re: 4., im: 2. };
-            newton_method_approximate(black_box(&pol), black_box(&dpol), black_box(&com), 100);
-        })
-    });
+    let mut group_a = c.benchmark_group("solve point");
+    for pol_order in 2..=7 {
+        group_a.bench_with_input(BenchmarkId::from_parameter(pol_order), &pol_order, |b, &pol_order| {
+            b.iter(|| {
+                let mut rng = rand::thread_rng();
+                let coef = (0..pol_order).map(|_| rng.gen()).collect();
+                let pol = Polynomial::new(coef);
+                let dpol = pol.derivative();
+                let point = Complex { re: 4., im: 2. };
+                newton_method_approximate(black_box(&pol), black_box(&dpol), black_box(&point), 100);
+            })
+        });
+    }
+    group_a.finish();
+
+    let mut group_f = c.benchmark_group("solve field");
+    group_f
+        .measurement_time(Duration::from_secs(10));
+    for grid in [16, 32, 64, 128, 256] {
+        group_f.bench_with_input(BenchmarkId::from_parameter(grid), &grid, |b, &grid| {
+            b.iter(|| {
+                // y = 2x^5 - 3x^4 + 7x^3 + 5x + 3
+                let pol = Polynomial::new(vec![3, 5, 0, 7, -3, 2]);
+                let field = Field {
+                    source: Complex { re: -5., im: -5. },
+                    size: 10.0,
+                    grid: grid,
+                };
+                newton_method_field(&black_box(pol), &black_box(field), 100);
+            })
+        });
+    }
+    group_f.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
